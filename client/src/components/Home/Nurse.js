580e61414ac6dbe9e739270d76f1d8cdec384c53
Pradyumna Donekal <praddonekal@gmail.com>
2022-04-20 22:43:30 -0400

    changed nurse homepage

--- client/src/components/Home/Nurse.js
+++ client/src/components/Home/Nurse.js
@@ -9,7 +9,14 @@ export default function Nurse(props) {
   return (
     <div>
       <NavBar />
-      <h1 className="Nurse">Welcome to Nurse</h1>
+      <br></br>
+      <div className="container-fluid col-6 justify-content-center margins">
+        <div className="span12 div-style p-10">
+          <div className="bg-danger text-light title">
+            <h2 className="h2-style">Nurse Homepage</h2>
+          </div>
+          </div>
+          </div>
     </div>
   );
 }
